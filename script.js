864f7d8a80f2c6b80e8fbbd1f062713675ce1e76
Desireeb <desireeb@sd-131966.dedibox.fr>
2020-11-16 15:44:20 +0100

    ajout des fonctions compareGuesses et updateScore

--- script.js
+++ script.js
@@ -2,5 +2,36 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
+// Generate a new target number
+const generateTarget = () => {
+    const randomTarget = Math.floor(Math.random() * 10);
+    return randomTarget;
+};
+
+// compareGuesses() determines which player is the closest to the target number
+let userGuess;
+let computerGuess;
+const targetNumber = generateTarget();
+
+const compareGuesses = (userGuess, computerGuess, targetNumber) => {
+  if (Math.abs(targetNumber-userGuess) <= Math.abs(targetNumber-computerGuess)) {
+    //player won
+    return true;
+  } else {
+    //computer won
+    return false;
+  }
+};
+
+// updateScore() add a point to the winner's score
+const updateScore = (winner) => {
+    if (winner === 'human') {
+      humanScore++;
+      console.log(humanScore);
+    } else {
+      computerScore++;
+    }
+  };
+  
+
 
